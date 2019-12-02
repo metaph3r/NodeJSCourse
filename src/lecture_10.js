3ce27d596e84fa7d882857a09cf99744c4d1d2a7
Silvio Glöckner <sgloeckner@proficom.de>
2019-12-02 12:07:24 +0100

    added source for lecture 10

---
+++ src/lecture_10.js
@@ -0,0 +1,8 @@
+// const add = require('./utils')
+// const sum = add(4, -2)
+const getNotes = require('./notes')
+const notes = getNotes()
+
+// console.log(name)
+// console.log(sum)
+console.log(notes)
