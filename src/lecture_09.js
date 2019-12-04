f2cda3266f83030d8f08c44bc1a0008b9a54d00d
Silvio Glöckner <sgloeckner@proficom.de>
2019-12-04 18:05:15 +0100

    delete old source files

--- src/lecture_09.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs')
-const filename = 'lecture_09.txt'
-
-// fs.writeFileSync(filename, 'My name is Silvio Glöckner')
-
-fs.appendFileSync(filename, 'This is a new line\n')
--- src/lecture_10.js
+++
@@ -1,8 +0,0 @@
-// const add = require('./utils')
-// const sum = add(4, -2)
-const getNotes = require('./notes')
-const notes = getNotes()
-
-// console.log(name)
-// console.log(sum)
-console.log(notes)
