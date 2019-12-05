24eeb0ff6fe432a6547d91696772079a2f055144
Silvio Glöckner <sgloeckner@proficom.de>
2019-12-05 07:39:38 +0100

    first weather app implementation

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,7 +1,17 @@
-console.log('Starting')
+const request = require('request')
 
-setTimeout(() => console.log('2 second timeout'), 2000)
+const url = 'https://api.darksky.net/forecast/03c5aad23649cbc65143c286189eb211/51.0492,13.7383?units=ca'
 
-setTimeout(() => console.log('0 second timeout'), 0)
+request({
+    url: url,
+    json: true
+}, (error, response, body) => {
+    if (error) {
+        console.log(error)
+    } else {
+        const temperature = body.currently.temperature
+        const precipitation = body.currently.precipProbability
 
-console.log('Stopping')
+        console.log('It is currently ' + temperature + ' degrees out. There is a ' + precipitation + '% probability of rain.')
+    }
+})
