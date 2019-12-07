ab5059fe292056f87faf751ee91ff0a725affcc3
Silvio Glöckner <sgloeckner@proficom.de>
2019-12-07 16:43:02 +0100

    put geocode method in separate module

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,6 +1,8 @@
 const request = require('request')
 
-const url = 'https://api.darksky.net/forecast/03c5aad23649cbc65143c286189eb211/51.0492,13.7383?units=ca'
+const geocode = require('./utils/geocode')
+
+// const url = 'https://api.darksky.net/forecast/03c5aad23649cbc65143c286189eb211/51.0492,13.7383?units=ca'
 
 // request({
 //     url: url,
@@ -20,23 +22,28 @@ const url = 'https://api.darksky.net/forecast/03c5aad23649cbc65143c286189eb211/5
 //     }
 // })
 
-const mapBoxUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Radebeul.json?access_token=pk.eyJ1IjoibWV0YXBoM3IiLCJhIjoiY2szc2R0ZjZxMDQwYTNkbzEzOTdrc3AyaSJ9.Ty1SQFwFJbcbd8cSTR6ThA&limit=1'
+// const mapBoxUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Radebeul.json?access_token=pk.eyJ1IjoibWV0YXBoM3IiLCJhIjoiY2szc2R0ZjZxMDQwYTNkbzEzOTdrc3AyaSJ9.Ty1SQFwFJbcbd8cSTR6ThA&limit=1'
 
-request({
-    url: mapBoxUrl,
-    json: true
-}, (error, response, body) => {
-    if (error) {
-        console.log('Unable to connect to location service!')
-    } else if (body.features.length === 0) {
-        console.log('Unable to find location!')
-    } else {
-        const feature = body.features[0] // get the feature information for the requested search term
-        const feature_name = feature.place_name
-        const lat = feature.center[1]
-        const lon = feature.center[0]
+// request({
+//     url: mapBoxUrl,
+//     json: true
+// }, (error, response, body) => {
+//     if (error) {
+//         console.log('Unable to connect to location service!')
+//     } else if (body.features.length === 0) {
+//         console.log('Unable to find location!')
+//     } else {
+//         const feature = body.features[0] // get the feature information for the requested search term
+//         const feature_name = feature.place_name
+//         const lat = feature.center[1]
+//         const lon = feature.center[0]
 
-        console.log('Coordinates for ' + feature_name)
-        console.log('Latitude: ' + lat + ' Longitude: ' + lon + '\n')
-    }
+//         console.log('Coordinates for ' + feature_name)
+//         console.log('Latitude: ' + lat + ' Longitude: ' + lon + '\n')
+//     }
+// })
+
+geocode('Radebeul', (error, response) => {
+    console.log(error)
+    console.log(response)
 })
