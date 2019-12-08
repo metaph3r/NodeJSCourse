5bd8f558f4c53f4199799b52563484261edebdd6
Silvio Glöckner <sgloeckner@proficom.de>
2019-12-08 15:56:39 +0100

    added dynamic pages

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -9,7 +9,24 @@ app.set('view engine', 'hbs')
 app.use(express.static(path.join(__dirname, '../public')))
 
 app.get('/', (req, res) => {
-    res.render('index')
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Silvio Glöckner'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About me',
+        name: 'Silvio Glöckner'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        message: 'Let me help you'
+    })
 })
 
 app.get('/weather', (req, res) => {
