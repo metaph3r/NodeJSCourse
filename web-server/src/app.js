46edc35a90520abb72afddcd11e3cb38a650f0e9
Silvio Glöckner <sgloeckner@proficom.de>
2019-12-10 18:53:41 +0100

    404 pages

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -48,6 +48,22 @@ app.get('/weather', (req, res) => {
     })
 })
 
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404 page not found',
+        message: 'Help page not found.',
+        name
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404 page not found',
+        message: 'Page not found.',
+        name
+    })
+})
+
 app.listen(3000, () => {
     console.log('Server is up on port 3000.')
 })
