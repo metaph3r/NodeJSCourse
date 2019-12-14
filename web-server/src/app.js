69f8ab993cb481f7fc3ebd89e9ce6b120f170628
Silvio Glöckner <sgloeckner@proficom.de>
2019-12-14 10:45:52 +0100

    the query string

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -42,9 +42,27 @@ app.get('/help', (req, res) => {
 })
 
 app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: 'Please provide an address'
+        })
+    }
     res.send({
         forecast: 'Cloudy with chance of rain',
-        location: 'Radebeul'
+        location: 'Radebeul',
+        address: req.query.address
+    })
+})
+
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term'
+        })
+    }
+
+    res.send({
+        products: []
     })
 })
 
