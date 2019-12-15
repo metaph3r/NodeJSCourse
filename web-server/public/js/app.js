eda7bc6062562a68adcca0e4626a50784e5ff301
Silvio Glöckner <sgloeckner@proficom.de>
2019-12-15 18:17:56 +0100

    Wiring up the User Interface

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,18 +1,24 @@
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
+
+    messageOne.textContent = 'Fetching weather information'
+    messageTwo.textContent = ''
 
     const location = search.value
 
     fetch('/weather?location=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
-                console.log(data.error)
+                messageOne.textContent = data.error
             } else {
-                console.log(data.location)
-                console.log(data.forecast)
+                messageOne.textContent = 'Weather for ' + data.location
+                messageTwo.textContent = data.forecast
             }
         })
     })
