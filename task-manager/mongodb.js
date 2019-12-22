e507d071181cbd5a6eb26ac6db0a7cad54a31918
Silvio Glöckner <silvio.gloeckner@outlook.de>
2019-12-22 18:25:31 +0100

    preparation for the next lecture

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -15,39 +15,5 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 
     const db = client.db(databaseName)
 
-    // db.collection('users').findOne({
-    //     _id: new ObjectID("5dff818166afe475fc83413c")
-    // }, (error, user) => {
-    //     if (error) throw error
-
-    //     console.log(user)
-    // })
-
-    // db.collection('users').find({
-    //     name: 'Silvio'
-    // }).toArray((error, users) => {
-    //     if (error) throw error
-
-    //     console.log(users)
-    // })
-
-    // db.collection('users').find({
-    //     name: 'Silvio'
-    // }).count((error, users) => {
-    //     if (error) throw error
-
-    //     console.log(users)
-    // })
-
-    db.collection('tasks').findOne({
-        _id: new ObjectID("5dff7ca75ea4db6cdcfec31d")
-    }, (error, task) => {
-        console.log(task)
-    })
-
-    db.collection('tasks').find({
-        completed: false
-    }).toArray((error, tasks) => {
-        console.log(tasks)
-    })
+    
 })
