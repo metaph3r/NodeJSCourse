d33e865ee8d7b02b8bb8019cd968f48a85adf94a
Silvio Glöckner <silvio.gloeckner@outlook.de>
2019-12-22 15:26:14 +0100

    added some more sample code

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
-const MONGODB_USERNAME = process.env.MONGODB_USERNAME
-const MONGODB_PASSWORD = process.env.MONGODB_PASSWORD
+const MONGODB_USERNAME = encodeURIComponent(process.env.MONGODB_USERNAME)
+const MONGODB_PASSWORD = encodeURIComponent(process.env.MONGODB_PASSWORD)
 
 const mongodb = require('mongodb')
 const MongoClient = mongodb.MongoClient
@@ -11,15 +11,48 @@ const connectionURL = 'mongodb://'
     + '@127.0.0.1:27017'
 const databaseName = 'task-manager'
 
-MongoClient.connect(connectionURL, {
-    useNewUrlParser: true
-}, (error, client) => {
+MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
     if (error) throw error
 
     const db = client.db(databaseName)
 
-    db.collection('users').insertOne({
-        name: 'Silvio',
-        age: 40
+    // db.collection('users').insertOne({
+    //     name: 'Silvio',
+    //     age: 40
+    // }, (error, result) => {
+    //     if (error) throw error
+
+    //     console.log(result.ops)
+    // })
+
+    // db.collection('users').insertMany([
+    //     {
+    //         name: 'Jen',
+    //         age: 28
+    //     }, {
+    //         name: 'Gunther',
+    //         age: 27
+    //     }
+    // ], (error, result) => {
+    //     if (error) throw error
+
+    //     console.log(result.ops)
+    // })
+
+    db.collection('tasks').insertMany([
+        {
+            description: 'Do stuff!',
+            completed: false
+        }, {
+            description: 'Learn Node.js',
+            completed: false
+        }, {
+            description: 'Install Ubuntu',
+            completed: true
+        }
+    ], (error, result) => {
+        if (error) throw error
+
+        console.log(result.ops)
     })
 })
