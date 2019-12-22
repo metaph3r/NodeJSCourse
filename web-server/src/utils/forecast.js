5cff244a0b89cd4304eb4b9804a72d791e17b02d
Silvio Glöckner <silvio.gloeckner@outlook.de>
2019-12-22 11:00:27 +0100

    added additional forecast information

--- web-server/src/utils/forecast.js
+++ web-server/src/utils/forecast.js
@@ -14,8 +14,10 @@ const forecast = (latitude, longitude, callback) => {
         } else {
             callback(undefined, {
                 forecast: body.daily.data[0].summary +
-                    ' It is ' + body.currently.temperature + ' degrees out and a ' +
-                    body.currently.precipProbability*100 + ' % chance of rain.'
+                    ' It is ' + body.currently.temperature + ' degrees out and a '
+                    + body.currently.precipProbability * 100 + ' % chance of rain.'
+                    + ' The expected highest temperature for today is ' + body.daily.data[0].temperatureHigh
+                    + ' degrees and the lowest is ' + body.daily.data[0].temperatureLow + ' degrees.'
             })
         }
     })
