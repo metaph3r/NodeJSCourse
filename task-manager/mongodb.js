22169ef292cc94e2f31d226211a736e48d590718
Silvio Glöckner <silvio.gloeckner@outlook.de>
2019-12-22 16:23:03 +0100

    Querying Documents

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -3,8 +3,7 @@ require('dotenv').config()
 const MONGODB_USERNAME = encodeURIComponent(process.env.MONGODB_USERNAME)
 const MONGODB_PASSWORD = encodeURIComponent(process.env.MONGODB_PASSWORD)
 
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
+const { MongoClient, ObjectID } = require('mongodb')
 
 const connectionURL = 'mongodb://'
     + MONGODB_USERNAME + ':' + MONGODB_PASSWORD
@@ -16,43 +15,39 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 
     const db = client.db(databaseName)
 
-    // db.collection('users').insertOne({
-    //     name: 'Silvio',
-    //     age: 40
-    // }, (error, result) => {
+    // db.collection('users').findOne({
+    //     _id: new ObjectID("5dff818166afe475fc83413c")
+    // }, (error, user) => {
     //     if (error) throw error
 
-    //     console.log(result.ops)
+    //     console.log(user)
     // })
 
-    // db.collection('users').insertMany([
-    //     {
-    //         name: 'Jen',
-    //         age: 28
-    //     }, {
-    //         name: 'Gunther',
-    //         age: 27
-    //     }
-    // ], (error, result) => {
+    // db.collection('users').find({
+    //     name: 'Silvio'
+    // }).toArray((error, users) => {
     //     if (error) throw error
 
-    //     console.log(result.ops)
+    //     console.log(users)
     // })
 
-    db.collection('tasks').insertMany([
-        {
-            description: 'Do stuff!',
-            completed: false
-        }, {
-            description: 'Learn Node.js',
-            completed: false
-        }, {
-            description: 'Install Ubuntu',
-            completed: true
-        }
-    ], (error, result) => {
-        if (error) throw error
+    // db.collection('users').find({
+    //     name: 'Silvio'
+    // }).count((error, users) => {
+    //     if (error) throw error
 
-        console.log(result.ops)
+    //     console.log(users)
+    // })
+
+    db.collection('tasks').findOne({
+        _id: new ObjectID("5dff7ca75ea4db6cdcfec31d")
+    }, (error, task) => {
+        console.log(task)
+    })
+
+    db.collection('tasks').find({
+        completed: false
+    }).toArray((error, tasks) => {
+        console.log(tasks)
     })
 })
