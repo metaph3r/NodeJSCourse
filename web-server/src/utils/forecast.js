c948e3c022bf4da2ff5506d98cc7321966a41446
Silvio Glöckner <silvio.gloeckner@outlook.de>
2019-12-22 11:11:07 +0100

    restructured the response message

--- web-server/src/utils/forecast.js
+++ web-server/src/utils/forecast.js
@@ -13,11 +13,14 @@ const forecast = (latitude, longitude, callback) => {
             callback(body.error)
         } else {
             callback(undefined, {
-                forecast: body.daily.data[0].summary +
-                    ' It is ' + body.currently.temperature + ' degrees out and a '
+                forecast: body.daily.data[0].summary
+                    + ' It is ' + body.currently.temperature
+                    + ' degrees outside. The expected highest temperature for today is '
+                    + body.daily.data[0].temperatureHigh
+                    + ' degrees and the lowest is '
+                    + body.daily.data[0].temperatureLow
+                    + ' degrees. There is a '
                     + body.currently.precipProbability * 100 + ' % chance of rain.'
-                    + ' The expected highest temperature for today is ' + body.daily.data[0].temperatureHigh
-                    + ' degrees and the lowest is ' + body.daily.data[0].temperatureLow + ' degrees.'
             })
         }
     })
