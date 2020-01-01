32b01dc5ca7be66c13dc7ef89e188cb5bd8a760e
Silvio Glöckner <silvio.gloeckner@outlook.de>
2020-01-01 17:23:44 +0100

    moved model for users to separate file

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,56 +1,16 @@
 require('dotenv').config()
 
-var mongoose = require('mongoose')
-var validator = require('validator')
+const mongoose = require('mongoose')
 
-const MONGODB_USERNAME = encodeURIComponent(process.env.MONGODB_USERNAME)
-const MONGODB_PASSWORD = encodeURIComponent(process.env.MONGODB_PASSWORD)
-
-const connectionURL = 'mongodb://' + MONGODB_USERNAME + ':' + MONGODB_PASSWORD + '@85.214.129.146:27017/task-manager-api?authSource=admin'
+const connectionURL = 'mongodb://'
+    + encodeURIComponent(process.env.MONGODB_USERNAME) + ':' 
+    + encodeURIComponent(process.env.MONGODB_PASSWORD) + '@'
+    + process.env.MONGODB_HOST + ':' + process.env.MONGODB_PORT
+    + '/task-manager-api?authSource=admin'
 
 mongoose.connect(connectionURL, {
     useNewUrlParser: true,
     useCreateIndex: true
-})
-
-// model for users
-const User = mongoose.model('User', {
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    age: {
-        type: Number,
-        default: 0,
-        validate(value) {
-            if(value < 0) {
-                throw new Error('Age must be a positive number')
-            }
-        }
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value) {
-            if(!validator.isEmail(value)) {
-                throw new Error('Email is invalid')
-            }
-        }
-    },
-    password: {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: 7,
-        validate(value) {
-            if(value.toLowerCase().includes('password')) {
-                throw new Error('Password cannot contain "password"')
-            }
-        }
-    }
 })
 
 // model for tasks
@@ -65,25 +25,3 @@ const Task = mongoose.model('Task', {
         default: false
     }
 })
-
-const task = new Task({
-    description: '            Do something!     '
-})
-
-task.save().then(() => {
-    console.log(task)
-}).catch((error) => {
-    console.error(error)
-})
-
-// const me = new User({
-//     name: '   Silvio  ',
-//     email: '   silvio@silvio-gloeckner.de   ',
-//     password: 'Password'
-// })
-
-// me.save().then(() => {
-//     console.log(me)
-// }).catch((error) => {
-//     console.error(error)
-// })
