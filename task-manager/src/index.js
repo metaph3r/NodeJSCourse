81497d6b8149f88c4cd54a804d03d21fdc0583f3
Silvio Glöckner <silvio.gloeckner@outlook.de>
2020-01-02 20:13:36 +0100

    added reading endpoints for tasks

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -47,6 +47,25 @@ app.post('/tasks', (req, res) => {
     })
 })
 
+app.get('/tasks', (req, res) => {
+    Task.find({}).then((tasks) => {
+        res.status(200).send(tasks)
+    }).catch((e) => {
+        res.status(500).send(e)
+    })
+})
+
+app.get('/tasks/:id', (req, res) => {
+    Task.findById(req.params.id).then((task) => {
+        if(!task) {
+            return res.send(404).send({})
+        }
+        res.status(200).send(task)
+    }).catch((e) => {
+        res.status(500).send(e)
+    })
+})
+
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
