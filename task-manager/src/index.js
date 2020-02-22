5ede48d4049509d52740252cd2597dcaf1baae3b
Silvio Glöckner <silvio.gloeckner@outlook.de>
2020-02-22 07:58:25 +0100

    delete resources

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -63,6 +63,22 @@ app.patch('/users/:id', async (req, res) => {
     }
 })
 
+app.delete('/users/:id', async (req, res) => {
+    const _id = req.params.id
+
+    try {
+        const user = await User.findByIdAndDelete(_id)
+
+        if(!user) {
+            return res.status(404).send({error: 'User with _id ' + _id + ' not found.'})
+        }
+
+        res.send(user)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
 app.post('/tasks', async (req, res) => {
     const task = new Task(req.body)
 
@@ -115,6 +131,22 @@ app.patch('/tasks/:id', async (req, res) => {
         res.send(task)
     } catch (error) {
         res.status(400).send(error)
+    }
+})
+
+app.delete('/tasks/:id', async (req, res) => {
+    const _id = req.params.id
+
+    try {
+        const task = await Task.findByIdAndDelete(_id)
+
+        if(!task) {
+            return res.status(404).send({error: 'Task with _id ' + _id + ' not found.'})
+        }
+
+        res.send(task)
+    } catch (error) {
+        res.status(500).send(error)
     }
 })
 
