edcf9fc06c0953e16e5ea7cdb10e8f1a3b718626
Silvio Glöckner <silvio.gloeckner@outlook.de>
2020-02-22 07:33:40 +0100

    resource updating

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -40,6 +40,29 @@ app.get('/users/:id', async (req, res) => {
     }
 })
 
+app.patch('/users/:id', async (req, res) => {
+    const _id = req.params.id
+    
+    // check of only allowed fields are updates
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email', 'password', 'age']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if(!isValidOperation) return res.status(400).send({ error: 'Invalid updates' })
+
+    try {
+        const user = await User.findByIdAndUpdate(_id, req.body, { new: true, runValidators: true })
+        
+        if (!user) {
+            return res.status(404).send({ error: 'User with id ' + _id + ' not found.' })
+        }
+
+        res.send(user)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
+
 app.post('/tasks', async (req, res) => {
     const task = new Task(req.body)
 
