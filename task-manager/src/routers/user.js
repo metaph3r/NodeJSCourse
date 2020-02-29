b7255626994acf2d8539e02777374cb5dae24f0f
Silvio Glöckner <silvio.gloeckner@outlook.de>
2020-02-29 10:03:50 +0100

    added user authentication

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -2,6 +2,8 @@ const express = require('express')
 const User = require('../models/user')
 const router = new express.Router()
 
+const auth = require('../middleware/auth')
+
 router.post('/users', async (req, res) => {
     const user = new User(req.body)
 
@@ -27,13 +29,8 @@ router.post('/users/login', async (req, res) => {
     }
 })
 
-router.get('/users', async (req, res) => {
-    try {
-        const users = await User.find()
-        res.status(200).send(users)
-    } catch (error) {
-        res.status(500).send(error)
-    }
+router.get('/users/me', auth, async (req, res) => {
+    res.send(req.user)
 })
 
 router.get('/users/:id', async (req, res) => {
