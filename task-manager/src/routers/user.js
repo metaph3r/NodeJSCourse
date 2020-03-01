38d1042e32075c100c659e632123b0d48f0d0839
Silvio Glöckner <silvio.gloeckner@outlook.de>
2020-03-01 19:26:23 +0100

    added logout

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -11,7 +11,7 @@ router.post('/users', async (req, res) => {
         await user.save()
 
         const token = await user.generateAuthToken()
-        
+
         res.status(201).send({ user, token })
     } catch (error) {
         res.status(400).send(error)
@@ -26,6 +26,32 @@ router.post('/users/login', async (req, res) => {
         res.send({ user, token })
     } catch (error) {
         res.status(400).send(error)
+    }
+})
+
+router.post('/users/logout', auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token
+        })
+
+        await req.user.save()
+
+        res.send()
+    } catch (error) {
+        res.status(500).send()
+    }
+})
+
+router.post('/users/logoutAll', auth, async (req, res) => {
+    try {
+        req.user.tokens = []
+
+        await req.user.save()
+
+        res.send()
+    } catch (error) {
+        res.status(500).send()
     }
 })
 
