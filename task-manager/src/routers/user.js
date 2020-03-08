609eb300f189ea7410583ef8dff74bc890dbea65
Silvio Glöckner <silvio.gloeckner@outlook.de>
2020-03-08 18:25:44 +0100

    serving up files

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -86,6 +86,21 @@ router.delete('/users/me/avatar', auth, async (req, res) => {
     res.send()
 })
 
+router.get('/users/:id/avatar', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
+
+        if(!user || !user.avatar) {
+            throw new Error()
+        }
+
+        res.set('Content-Type', 'image/jpg')
+        res.send(user.avatar)
+    } catch (error) {
+        res.status(404).send()
+    }
+})
+
 router.patch('/users/me', auth, async (req, res) => {
     // check of only allowed fields are updates
     const updates = Object.keys(req.body)
