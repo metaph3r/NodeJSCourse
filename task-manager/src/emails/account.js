28819ef2e9442b005ca788a3620ca7d1fed6326b
Silvio Glöckner <silvio.gloeckner@outlook.de>
2020-03-16 06:59:20 +0100

    added smtp port as environment variable

--- task-manager/src/emails/account.js
+++ task-manager/src/emails/account.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const nodemailer = require('nodemailer')
 const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
-    port: 587,
+    port: process.env.SMTP_PORT,
     secure: false,
     auth: {
         user: process.env.SMTP_USER,
