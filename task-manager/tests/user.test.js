b3ea08b5988b1efd8d414408ae86a962b10e6184
Silvio Glöckner <silvio.gloeckner@outlook.de>
2020-03-30 18:59:00 +0200

    added more tests

--- task-manager/tests/user.test.js
+++ task-manager/tests/user.test.js
@@ -3,6 +3,18 @@ process.env.MONGODB_DATABASE = 'task-manager-api-test'
 
 const request = require('supertest')
 const app = require('../src/app')
+const User = require('../src/models/user')
+
+const userOne = {
+    name: 'Foo Bar',
+    email: 'foo@bar.com',
+    password: '56what!!'
+}
+
+beforeEach(async () => {
+    await User.deleteMany()
+    await new User(userOne).save()
+})
 
 test('Should signup a new user', async () => {
     await request(app).post('/users')
@@ -12,4 +24,20 @@ test('Should signup a new user', async () => {
             password: 'MyPass777!'
         })
         .expect(201)
+})
+
+test('Should login existing user', async () => {
+    await request(app).post('/users/login').send({
+        email: userOne.email,
+        password: userOne.password
+    })
+    .expect(200)
+})
+
+test('Should not login nonexistent user', async () => {
+    await request(app).post('/users/login').send({
+        email: 'wrong@example.com',
+        password: 'foobar'
+    })
+    .expect(401)
 })
