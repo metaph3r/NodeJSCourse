f78948854f17dcdcdcefdc6d2ae9ce684793aaea
Silvio Glöckner <silvio.gloeckner@outlook.de>
2020-04-02 15:13:32 +0200

    added tests

--- task-manager/tests/user.test.js
+++ task-manager/tests/user.test.js
@@ -12,6 +12,7 @@ const userOne = {
     name: 'Foo Bar',
     email: 'foo@bar.com',
     password: '56what!!',
+    age: 40,
     tokens: [{
         token: jwt.sign({ _id: userOneId }, process.env.JWT_SECRET)
     }]
@@ -98,4 +99,34 @@ test('Should not delete account for unauthenticated user', async () => {
         .delete('/users/me')
         .send()
         .expect(401)
+})
+
+test('Should upload avatar image', async () => {
+    await request(app)
+        .post('/users/me/avatar')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .attach('avatar', 'tests/fixtures/profile-pic.jpg')
+        .expect(200)
+    const user = await User.findById(userOneId)
+    expect(user.avatar).toEqual(expect.any(Buffer))
+})
+
+test('Should update valid user fields', async () => {
+    await request(app)
+        .patch('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send({ name: 'Mike' })
+        .expect(200)
+    const user = await User.findById(userOneId)
+    expect(user.name).toBe('Mike')
+})
+
+test('Should not update invalid user fields', async () => {
+    await request(app)
+        .patch('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send({ age: -1 })
+        .expect(400)
+    const user = await User.findById(userOneId)
+    expect(user.age).toBe(40)
 })
