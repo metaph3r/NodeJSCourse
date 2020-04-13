798104bcc25e5bd14d3850625d791706cbd64677
Silvio Glöckner <silvio.gloeckner@outlook.de>
2020-04-13 11:19:39 +0200

    added some more tests for tasks

--- task-manager/tests/task.test.js
+++ task-manager/tests/task.test.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const request = require('supertest')
 const app = require('../src/app')
 const Task = require('../src/models/task')
-const { userOneId, userOne, setupDatabase } = require('./fixtures/db')
+const { userOne, userTwo, taskOne, setupDatabase } = require('./fixtures/db')
 
 beforeEach(setupDatabase)
 
@@ -16,7 +16,28 @@ test('Should create task for user', async () => {
         })
         .expect(201)
 
-        const task = await Task.findById(response.body._id)
-        expect(task).not.toBeNull()
-        expect(task.completed).toEqual(false)
+    const task = await Task.findById(response.body._id)
+    expect(task).not.toBeNull()
+    expect(task.completed).toEqual(false)
+})
+
+test('Should get all tasks for user one', async () => {
+    const response = await request(app)
+        .get('/tasks')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send()
+        .expect(200)
+
+    expect(response.body.length).toEqual(2)
+})
+
+test('Should not delete other users tasks', async () => {
+    await request(app)
+        .delete(`/tasks/${taskOne._id}`)
+        .set('Authorization', `Bearer ${userTwo.tokens[0].token}`)
+        .send()
+        .expect(404)
+
+    const task = await Task.findById(taskOne._id)
+    expect(task).not.toBeNull()
 })
