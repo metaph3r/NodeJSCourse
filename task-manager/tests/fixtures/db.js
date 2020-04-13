f428f3ee7b9713125f53572b25953259c0e9af4a
Silvio Glöckner <silvio.gloeckner@outlook.de>
2020-04-13 10:49:26 +0200

    added tests for task

---
+++ task-manager/tests/fixtures/db.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+const jwt = require('jsonwebtoken')
+const User = require('../../src/models/user')
+
+const userOneId = new mongoose.Types.ObjectId()
+const userOne = {
+    _id: userOneId,
+    name: 'Foo Bar',
+    email: 'foo@bar.com',
+    password: '56what!!',
+    age: 40,
+    tokens: [{
+        token: jwt.sign({ _id: userOneId }, process.env.JWT_SECRET)
+    }]
+}
+
+const setupDatabase = async () => {
+    await User.deleteMany()
+    await new User(userOne).save()
+}
+
+module.exports = {
+    userOneId,
+    userOne,
+    setupDatabase
+}
--- task-manager/tests/user.test.js
+++ task-manager/tests/user.test.js
@@ -1,27 +1,11 @@
 require('dotenv').config()
 
 const request = require('supertest')
-const jwt = require('jsonwebtoken')
-const mongoose = require('mongoose')
 const app = require('../src/app')
 const User = require('../src/models/user')
+const { userOneId, userOne, setupDatabase } = require('./fixtures/db')
 
-const userOneId = new mongoose.Types.ObjectId()
-const userOne = {
-    _id: userOneId,
-    name: 'Foo Bar',
-    email: 'foo@bar.com',
-    password: '56what!!',
-    age: 40,
-    tokens: [{
-        token: jwt.sign({ _id: userOneId }, process.env.JWT_SECRET)
-    }]
-}
-
-beforeEach(async () => {
-    await User.deleteMany()
-    await new User(userOne).save()
-})
+beforeEach(setupDatabase)
 
 test('Should signup a new user', async () => {
     const response = await request(app).post('/users')
